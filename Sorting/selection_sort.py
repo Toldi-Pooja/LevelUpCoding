1a7a1fa9c1e7d9c8bafb646ed87e64a70febfe76
Toldi-Pooja <163871349+Toldi-Pooja@users.noreply.github.com>
2026-02-10 19:09:37 +0530

    Add files via upload

---
+++ Sorting/selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(nums):
+    n = len(nums)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if nums[j] < nums[min_index]:
+                min_index = j
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+    return nums
+nums = list(map(int,input().split()))
+print(selection_sort(nums))
